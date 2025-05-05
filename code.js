4b557e35532be5a4a1f7804d92c8951834f4baa6
chenry900 <158115154+chenry900@users.noreply.github.com>
2025-05-05 16:37:21 -0600

    Update code.js

--- code.js
+++ code.js
@@ -6,5 +6,38 @@ function permutationSort(a) {
 
 function sort(array, num, perm) {
    var sorted = false;
-    
+
+   if (num == array.length) {
+       perm += 1;
+       sorted = check(array);
+       if (sorted == true) {
+           return perm;
+       }
+   }
+
+   for (var i = num; i < array.length; i++) {
+       swap(array, num, i);
+       sorted = sort(array, num + 1, perm);
+       sorted = check(array);
+       if (sorted == true) {
+           return perm;
+       }
+       swap(array, num, i);
+   }
+}
+
+function check(array) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] > array[(i + 1)]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function swap(array, num, i) {
+    var temp = array[num];
+    array[num] = array[i];
+    array[i] = temp;
+}
 }
